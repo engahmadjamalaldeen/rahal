dd44cb5b953f9cbb8dac88eadbc4f90df2e1ce5f
engahmadjamalaldeen <qqsy589@gmail.com>
2022-12-25 16:41:56 +0200

    'init11'

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -19,13 +19,13 @@ export const typeOrmConfig : TypeOrmModuleOptions = {
     ssl: {
         rejectUnauthorized: false,
       },
-      url: 'postgres://vnhaauvcffwinb:4a9df12d227133ed840a05539ab59d5902d0a72e380694f979254c9d412f64e6@ec2-54-155-110-181.eu-west-1.compute.amazonaws.com:5432/d9s9trld8fis1c',
-      type: 'postgres',
-      host: 'ec2-54-155-110-181.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      username: 'vnhaauvcffwinb',
-      password: '4a9df12d227133ed840a05539ab59d5902d0a72e380694f979254c9d412f64e6',
-      database: 'd9s9trld8fis1c',
+    url: 'postgresql://dTghyKAULiEmagxnakRJPbklQWYDEdwY:TiuTlXxiwxTjcAYxMzXyEhmXlOZpTHwp@db.thin.dev/f050b5c7-f16f-419f-85fc-1f69e1a3e80d',
+    type: 'postgres',
+    host: 'db.thin.dev',
+    port: 5432,
+    username: 'dTghyKAULiEmagxnakRJPbklQWYDEdwY',
+    password: 'TiuTlXxiwxTjcAYxMzXyEhmXlOZpTHwp',
+    database: 'f050b5c7-f16f-419f-85fc-1f69e1a3e80d',
     entities: [City, Blog, CityImage, Customer, Membership, Place, Trip, Reservation, ReservationType, Amenity, Review, Interest, Category, Room, MembershipUser],
     synchronize: true
 }
