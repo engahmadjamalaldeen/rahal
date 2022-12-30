e095e1180cec00df7e847d1d03843637fa2336f6
engahmadjamalaldeen <qqsy589@gmail.com>
2022-12-30 16:14:05 +0200

    updateCoustomer4

--- src/customers/customers.controller.ts
+++ src/customers/customers.controller.ts
@@ -24,7 +24,7 @@ export class CustomersController {
     addCustomer(@Body() createCustomerDto: CreateCustomerDto): Promise<Customer> {
         return this.customersService.addCustomer(createCustomerDto);
     }
-    @Post('/uodateCustomer')
+    @Post('/updateCustomer')
     @UsePipes(ValidationPipe)
     updateCustomer(@Body() createCustomerDto: CreateCustomerDto): Promise<Customer> {
         return this.customersService.updateCustomer(createCustomerDto);
