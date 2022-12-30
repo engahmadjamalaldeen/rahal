e62d60f072d87b26ff37c0bb32d4dcc506cb4fda
engahmadjamalaldeen <qqsy589@gmail.com>
2022-12-30 19:05:51 +0200

    api need city object with customer reservations4

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -117,7 +117,7 @@ export class CustomersService {
 
     async getCustomerById(id: number){
         const found = await this.customerRepository.findOne({ where: { id: id },
-             relations: ['reservations', 'reservations.place',  'reservations.room', 'tripsCreated', 'interests'] });
+             relations: ['reservations', 'reservations.place', 'place.city',  'reservations.room', 'tripsCreated', 'interests'] });
   
         // const place = await this.placeRepository.findOne({ where: { id: found[0].places[0].id }});
         // return place;
